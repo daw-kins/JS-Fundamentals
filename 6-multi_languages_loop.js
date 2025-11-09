4106fe2e921d91753bb3a9435e21c1aa031ced6d
Dawkins Omondi <omondi.kins@gmail.com>
2025-11-09 22:09:29 +0300

    7th commit

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+// Array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Initialize index
+let i = 0;
+
+// Use a loop to print each line in a single console.log
+while (i < languages.length) {
+    console.log(languages[i]);
+    i++;
+}

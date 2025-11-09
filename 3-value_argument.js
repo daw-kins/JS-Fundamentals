a87cbfc17844c951a67d508b3ddd431618754a32
Dawkins Omondi <omondi.kins@gmail.com>
2025-11-09 21:59:12 +0300

    fourth commit

---
+++ 3-value_argument.js
@@ -0,0 +1,9 @@
+// Get the command-line arguments (excluding 'node' and script name)
+const args = process.argv.slice(2);
+
+// Check if the first argument exists
+if (args[0] === undefined) {
+    console.log("No argument");
+} else {
+    console.log(args[0]);
+}
